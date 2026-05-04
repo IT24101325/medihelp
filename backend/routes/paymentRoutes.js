918df745c1de444d2b84b42c75cb6b35aec48404
IT24101325 <it24101325@my.sliit.lk>
2026-05-04 09:52:00 +0530

    Updated Frontend

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const {
-  createPayment,
   getPatientPayments,
   getDoctorPayments,
   getAllPayments,
   markPaid,
   getPaymentByAppointment,
+  getMyPayments,
+  payPayment,
 } = require('../controllers/paymentController');
 const { protect, adminOnly, doctorOnly } = require('../middleware/authMiddleware');
 
 router.use(protect);
 
-router.post('/create', createPayment);
+router.put('/:id/pay', payPayment);
 router.get('/patient/:id', getPatientPayments);
 router.get('/doctor/:id', doctorOnly, getDoctorPayments);
 router.get('/appointment/:appointmentId', getPaymentByAppointment);
+router.get('/my', getMyPayments);
 router.get('/', adminOnly, getAllPayments);
 router.put('/:id/mark-paid', doctorOnly, markPaid);
 
